5047f11b6ca92bf42fd6392f338f4d4700923934
wakishoo <14de063n@rikkyo.ac.jp>
2018-05-26 07:34:59 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,15 @@
 Rails.application.routes.draw do
-  get 'carts/show'
-  get 'carts/create'
-  get 'carts/apdate'
-  get 'carts/purchase'
-  get 'solds/index'
-  get 'solds/show'
-  get 'genres/index'
-  get 'genres/show'
-  get 'genres/edit'
-  get 'genres/update'
-  get 'genres/destoy'
-  get 'songs/new'
-  get 'songs/create'
-  get 'cds/show'
-  get 'cds/index'
-  get 'admins/top'
-  get 'admins/edit'
-  get 'users/show'
-  get 'users/edit'
-  devise_for :admins
+ devise_for :admins
   devise_for :users
+  resources :carts
+  resources :solds
+  resources :genres
+  resources :songs
+  resources :cds
+  resources :admins
+  resources :users
+
+  get 'admins/top' => 'admins#top'
+  get 'carts/purchase' => 'carts#purchase'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
