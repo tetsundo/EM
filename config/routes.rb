0235250ce408743a724e9e577ee77853036c2920
ryysn0314 <ryo_yosshi@hotmail.co.jp>
2018-05-26 08:10:01 +0000

    トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :cds
   resources :admins
   resources :users
-
+  root 'cds#index'
   get 'admins/top' => 'admins#top'
   get 'carts/purchase' => 'carts#purchase'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
