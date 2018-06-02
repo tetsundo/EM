63f2edc84903088fe9a07ceeb6dc9a6afbe26d81
wakishoo <14de063n@rikkyo.ac.jp>
2018-06-02 06:13:48 +0000

    アプリケーションコントローラとアプリケーションhtml 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+	def after_sign_in_path_for(resource)
+      cds_path
+  end
+
+   def after_sign_out_path_for(resource)
+    	cds_path
+   end
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :tell_number, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
 end
