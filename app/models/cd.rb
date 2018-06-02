7bf4b591c871c38dbc17752f3394a19d2d922fee
ryysn0314 <ryo_yosshi@hotmail.co.jp>
2018-06-02 05:28:38 +0000

    アソシエーション

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -2,6 +2,6 @@ class Cd < ApplicationRecord
 	has_many :songs, dependent: :destroy, inverse_of: :cd
 	attachment :image
 	accepts_nested_attributes_for :songs, allow_destroy: true
-	belongs_to :genre
-	has_many :cart_item
+	# belongs_to :genre
+	# has_many :cart_item
 end
