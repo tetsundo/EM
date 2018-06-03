30fd384ed8185710b4120ff520ea67bc3a472abb
wakishoo <14de063n@rikkyo.ac.jp>
2018-06-03 05:54:42 +0000

    サインアップがんばっったけど、ログインが認証されなくなったよ⭐︎

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,9 +46,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :tell_number, :email, :post_code])
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
