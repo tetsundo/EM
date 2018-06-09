c14db8b8b3d660c277d0dd776071b75c47ef828d
wakishoo <14de063n@rikkyo.ac.jp>
2018-06-09 07:14:33 +0000

    論理削除導入。モデルのuser.rbをいじって、カラムも追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_111432) do
+ActiveRecord::Schema.define(version: 2018_06_09_070630) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2018_06_01_111432) do
     t.integer "post_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "soft_destroyed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
