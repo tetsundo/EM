74bfa0b2a12202a12c82e826c3642d5fb95a5d4c
ryysn0314 <ryo_yosshi@hotmail.co.jp>
2018-06-15 12:53:59 +0000

    在庫管理

--- app/controllers/solds_controller.rb
+++ app/controllers/solds_controller.rb
@@ -14,8 +14,11 @@ class SoldsController < ApplicationController
   		@sold_item.price = cart_item.price
   		@sold_item.sold_id = @sold.id
   		@sold_item.save
+      cart_item.cd.remaining_quantity -= cart_item.quantity
+      cart_item.cd.save
   		cart_item.destroy
   	end
+    redirect_to root_path
   end
 
   def index
