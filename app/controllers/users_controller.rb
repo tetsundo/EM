f0f610199866d95d95ed25e92345f1b27790a9f8
tetsundo <tetsundo0156justdoit@gmail.com>
2018-06-20 12:01:56 +0000

    投稿日時順に並び替えた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-    @solds = @user.solds
+    @solds = @user.solds.order(created_at: 'desc')
   end
 
   def edit
